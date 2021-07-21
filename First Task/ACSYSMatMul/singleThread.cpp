cf0e65acb8ed1ac6a85ce3f1308473194c74b91b
JeeHyun Kim <jeehyun990@yonsei.ac.kr>
2021-07-21 20:15:26 +0900

    singleThread~ing

--- First Task/ACSYSMatMul/singleThread.cpp
+++ First Task/ACSYSMatMul/singleThread.cpp
@@ -1,7 +1,5 @@
 #include <iostream>
 #include <array>
-#include <algorithm>
-#define MATSIZE 2048
 
 using namespace std;
 
@@ -15,23 +13,41 @@ int** makeMAT(int n) {
 			mat[r][c] = rand() % 10 - 4;
 		}
 	}
+	return mat;
+}
+
+int** makeZero(int n) {
+	int** mat = new int*[n];
+	for (int i = 0; i < n ;i++) {
+		mat[i] = new int[n];
+	}
+	for (int r = 0; r < n; r++) {
+		for (int c = 0; c < n ; c++) {
+			mat[r][c] = 0;
+		}
+	}
+	return mat;
+}
+
+int** matmul1(int** m1, int** m2) {
+	int n = sizeof(m1);
+	int** ret = makeZero(n);
+	for (int r = 0; r < n; r++) {
+		
+	}
+
+
+
+
+
+	return ret;
 }
 
 int main() {
 	// initiallize
-	int** mat1 = new int*[MATSIZE];
-	int** mat2 = new int*[MATSIZE];
-	int** mat3 = new int*[MATSIZE];
-	for (int i = 0 ; i < MATSIZE; i++) {
-		mat1[i] = new int[MATSIZE];
-		mat2[i] = new int[MATSIZE];
-		mat3[i] = new int[MATSIZE];
-	}
-	for (int r = 0; r < MATSIZE; r++) {
-		for (int c = 0; c < MATSIZE; c++) {
-			mat1[r][c] = rand() % 10 - 4;
-			mat2[r][c] = rand() % 10 - 4;
-			mat3[r][c] = 0;
-		}
-	}
+	int** mat1 = makeMAT(2048);
+	int** mat2 = makeMAT(2048);
+	int** mat3 = makeZero(2048);
+
+
 }
