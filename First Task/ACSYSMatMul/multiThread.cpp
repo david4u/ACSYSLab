711ae360f9f3239c71241e38f0e8e08724ddecbd
JeeHyun Kim <jeehyun990@yonsei.ac.kr>
2021-07-31 21:31:33 +0900

    feedback

--- First Task/ACSYSMatMul/multiThread.cpp
+++ First Task/ACSYSMatMul/multiThread.cpp
@@ -3,6 +3,13 @@
 #include <time.h>
 #include <vector>
 
+// init, alloc 분리
+// 뻔한 data로 singlethread검증
+// 복잡한 data로 multithread검증
+// mat 다양하게 자르기
+// makefile
+//.o * .o -> exe
+
 using namespace std;
 
 int** makeMAT(int n) {
@@ -12,7 +19,7 @@ int** makeMAT(int n) {
 	}
 	for (int r = 0; r < n; r++) {
 		for (int c = 0; c < n; c++) {
-			mat[r][c] = rand() % 10 - 4;
+			mat[r][c] = rand() % 10 - 4; //수식으로
 		}
 	}
 	return mat;
@@ -31,7 +38,7 @@ int** makeZero(int n) {
 	return mat;
 }
 
-void multiply(const int** m1, const int** m2, int** ret, int n, int startrow, int endrow, int startcol, int endcol) {
+void multiply(int** m1,int** m2, int** ret, int n, int startrow, int endrow, int startcol, int endcol) {
     for (int r = startrow; r < endrow; r++) {
         for (int c = startcol; c < endcol; c++) {
             for (int i = 0; i < n; i++) {
@@ -40,7 +47,6 @@ void multiply(const int** m1, const int** m2, int** ret, int n, int startrow, in
         }
     }
 }
-
 
 int** matmul2(int** m1, int** m2, int n) { // n -> original size
     int** ret = makeZero(n);
@@ -54,6 +60,7 @@ int** matmul2(int** m1, int** m2, int n) { // n -> original size
         c1 = n/4;
         c2 = c1*2;
         c3 = c1*3;
+
         threads.emplace_back(multiply, m1, m2, ret, n, 0, r1, 0, c1);
         threads.emplace_back(multiply, m1, m2, ret, n, r1, r2, c1, c2);
         threads.emplace_back(multiply, m1, m2, ret, n, r2, r3, c2, c3);
@@ -80,7 +87,7 @@ int main() {
     int** mat3 = matmul2(mat1, mat2, 2048);
     // FIXEND
 	end = clock();
-	cout << "Time : " << (double)(end - start) << " ms. \n";
+	cout << "Time : " << (double)(end - start) << " ns. \n";
 	for (int i = 0; i < 2048; i++) delete[] mat1[i];
 	for (int i = 0; i < 2048; i++) delete[] mat2[i];
 	for (int i = 0; i < 2048; i++) delete[] mat3[i];
