38235aacdffcb244e6f812b7b8e0ab4811a4e728
JeeHyun Kim <jeehyun990@yonsei.ac.kr>
2021-08-13 15:30:37 +0900

    transpose edit

--- First Task/ACSYSMatMul/single.cpp
+++ First Task/ACSYSMatMul/single.cpp
@@ -16,14 +16,14 @@ void Single::transmatmul(int** m1, int** m2, int** ret, int n) {
     int** m3 = new int*[n];
     for (int i = 0 ;i < n; i++) {
         m3[i] = new int[n];
-        for (int j = 0; j < n ;j++) {
-            m3[i][j] = m2[j][i];
+        for (int  j= 0; j < n; j++) {
+            m3[i][j] = m2[j][i]
         }
     }
     for (int r = 0; r < n ; r++) {
         for (int c =0; c < n; c++) {
             for (int i = 0; i < n; i++) {
-                ret[r][c] += m1[r][i] * m3[r][i];
+                ret[r][c] += m1[r][i] * m3[c][i];
             }
         }
     }
